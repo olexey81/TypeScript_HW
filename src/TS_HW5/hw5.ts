3c9069640045bbc29bba4bc0b8c4d3539a680014
Oleksii Kryvchykov <olexey81@gmail.com>
2024-01-07 21:27:42 +0200

    with_corrections

--- src/TS_HW5/hw5.ts
+++ src/TS_HW5/hw5.ts
@@ -23,10 +23,25 @@ enum Names {
 abstract class Figure {
   protected abstract readonly Color: Colors;
   protected abstract readonly Name: Names;
-  protected abstract CalculateArea(numberA: number, numberB?: number, numberC?: number): number;
+  protected abstract CalculateArea(): number;
+}
+
+abstract class FigureWithPrint extends Figure {
+  protected abstract Print(): string;
 }
 
 class Circle extends Figure {
+  private _radius: number = 0;
+
+  public get radius(): number {
+    return this._radius;
+  }
+
+  public set radius(r: number) {
+    if (!Validator.isPositive(r)) throw new RangeError('Radius is less or equal to zero!');
+    this._radius = r;
+  }
+
   constructor(
     public readonly Color: Colors,
     public readonly Name: Names
@@ -34,13 +49,23 @@ class Circle extends Figure {
     super();
   }
 
-  public CalculateArea(radius: number): number {
-    if (!(radius > 0)) throw new RangeError('Radius is less or equal to zero!');
-    return Math.PI * Math.pow(radius, 2);
+  public CalculateArea(): number {
+    return Math.PI * Math.pow(this._radius, 2);
   }
 }
 
-class Rectangle extends Figure {
+class Rectangle extends FigureWithPrint {
+  private _dimensions: [A: number, B: number] = [0, 0];
+
+  get dimensions(): [A: number, B: number] {
+    return this._dimensions;
+  }
+
+  set dimensions(d: [A: number, B: number]) {
+    if (!Validator.isPositive(d[0], d[1])) throw new RangeError('One or both dimensions less or equal to zero!');
+    this._dimensions = d;
+  }
+
   constructor(
     public readonly Color: Colors,
     public readonly Name: Names
@@ -48,10 +73,8 @@ class Rectangle extends Figure {
     super();
   }
 
-  public CalculateArea(dimensionA: number, dimensionB: number): number {
-    if (!Validator.isPositive(dimensionA, dimensionB))
-      throw new RangeError('One or both dimensions less or equal to zero!');
-    return dimensionA * dimensionB;
+  public CalculateArea(): number {
+    return this._dimensions[0] * this._dimensions[1];
   }
 
   public Print(): string {
@@ -59,7 +82,18 @@ class Rectangle extends Figure {
   }
 }
 
-class Square extends Figure {
+class Square extends FigureWithPrint {
+  private _dimension: number = 0;
+
+  public get dimension(): number {
+    return this._dimension;
+  }
+
+  public set dimension(d: number) {
+    if (!Validator.isPositive(d)) throw new RangeError('Dimension is less or equal to zero!');
+    this._dimension = d;
+  }
+
   constructor(
     public readonly Color: Colors,
     public readonly Name: Names
@@ -67,9 +101,8 @@ class Square extends Figure {
     super();
   }
 
-  public CalculateArea(dimension: number): number {
-    if (!Validator.isPositive(dimension)) throw new RangeError('Dimension is less or equal to zero!');
-    return Math.pow(dimension, 2);
+  public CalculateArea(): number {
+    return Math.pow(this._dimension, 2);
   }
 
   public Print(): string {
@@ -78,6 +111,18 @@ class Square extends Figure {
 }
 
 class Triangle extends Figure {
+  private _dimensions: [A: number, B: number, C: number] = [0, 0, 0];
+
+  get dimensions(): [A: number, B: number, C: number] {
+    return this._dimensions;
+  }
+
+  set dimensions(d: [A: number, B: number, C: number]) {
+    if (!Validator.isPositive(d[0], d[1], d[2])) throw new RangeError('One or all dimensions less or equal to zero!');
+    if (!Validator.isDimensionsCorrect(d[0], d[1], d[2])) throw new RangeError('Invalid triangle dimensions!');
+    this._dimensions = d;
+  }
+
   constructor(
     public readonly Color: Colors,
     public readonly Name: Names
@@ -85,24 +130,21 @@ class Triangle extends Figure {
     super();
   }
 
-  public CalculateArea(dimensionA: number, dimensionB: number, dimensionC: number): number {
-    if (!Validator.isPositive(dimensionA, dimensionB, dimensionC))
-      throw new RangeError('One or both dimensions less or equal to zero!');
-    if (!Validator.isDimensionsCorrect(dimensionA, dimensionB, dimensionC))
-      throw new RangeError('Invalid triangle dimensions!');
-    const semiPerimetr: number = (dimensionA + dimensionB + dimensionC) / 2;
+  public CalculateArea(): number {
+    const semiPerimetr: number = (this._dimensions[0] + this._dimensions[1] + this._dimensions[2]) / 2;
     const underRoot: number =
-      semiPerimetr * (semiPerimetr - dimensionA) * (semiPerimetr - dimensionB) * (semiPerimetr - dimensionC);
+      semiPerimetr *
+      (semiPerimetr - this._dimensions[0]) *
+      (semiPerimetr - this._dimensions[1]) *
+      (semiPerimetr - this._dimensions[2]);
     if (underRoot < 0) throw new EvalError('Invalid evaluation!');
     return Math.sqrt(underRoot);
   }
 }
 
 class Validator {
-  static isPositive(numberA: number, numberB?: number, numberC?: number): boolean {
-    if (numberB && numberC) return numberA > 0 && numberB > 0 && numberC > 0;
-    if (numberB) return numberA > 0 && numberB > 0;
-    return numberA > 0;
+  static isPositive(...numbers: number[]): boolean {
+    return numbers.filter(n => n <= 0).length === 0;
   }
 
   static isDimensionsCorrect(dimensionA: number, dimensionB: number, dimensionC: number): boolean {
@@ -114,14 +156,18 @@ class Validator {
   }
 }
 
-// const circle: Circle = new Circle(Colors.Yellow, Names.Circle);
-// console.log(`${circle.Color}\n${circle.Name}\n${circle.CalculateArea(0.16)}\n\n`);
+const circle: Circle = new Circle(Colors.Yellow, Names.Circle);
+circle.radius = 10;
+console.log(`${circle.Color}\n${circle.Name}\n${circle.CalculateArea()}\n\n`);
 
-// const rectangle: Rectangle = new Rectangle(Colors.LigthBlue, Names.Rectangle);
-// console.log(`${rectangle.Color}\n${rectangle.Name}\n${rectangle.CalculateArea(0.16, 0.16)}\n${rectangle.Print()}\n\n`);
+const rectangle: Rectangle = new Rectangle(Colors.LigthBlue, Names.Rectangle);
+rectangle.dimensions = [2, 4];
+console.log(`${rectangle.Color}\n${rectangle.Name}\n${rectangle.CalculateArea()}\n${rectangle.Print()}\n\n`);
 
-// const square: Square = new Square(Colors.Red, Names.Square);
-// console.log(`${square.Color}\n${square.Name}\n${square.CalculateArea(0.16)}\n${square.Print()}\n\n`);
+const square: Square = new Square(Colors.Red, Names.Square);
+square.dimension = 10;
+console.log(`${square.Color}\n${square.Name}\n${square.CalculateArea()}\n${square.Print()}\n\n`);
 
-// const triangle: Triangle = new Triangle(Colors.Green, Names.Triangle);
-// console.log(`${triangle.Color}\n${triangle.Name}\n${triangle.CalculateArea(6, 5, 5)}`);
+const triangle: Triangle = new Triangle(Colors.Green, Names.Triangle);
+triangle.dimensions = [6, 5, 5];
+console.log(`${triangle.Color}\n${triangle.Name}\n${triangle.CalculateArea()}`);
